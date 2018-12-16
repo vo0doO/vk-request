060f2204f20867edd189064c97c4d01f2669d2da
vo0doo <kirsanov.bvt@gmail.com>
2018-12-16 04:52:51 +0300

    добавлен логер

--- message_sender.py
+++ message_sender.py
@@ -3,9 +3,10 @@ import vk_api
 import sqlite3
 from numpy import random
 import logging
+import sys
 
 
-
+logger = logging.getLogger(__name__)
 
 
 USER_LOGIN = "+79319629413"
@@ -19,7 +20,6 @@ def sender_my_mesages():
     conn.row_factory = sqlite3.Row
     sql = "SELECT * FROM `kirishiVkUsers` WHERE `message_send` isnull"
     login, password, api_version, app_id, client_secret = '+79319629413', 'e31f567b', '5.73', '6009351', '415c746e415c746e415c746ee54107c6694415c415c746e1858a90711ebafe24fd0637f'
-    logger = logging.getLogger("vk_api")
     vk_session = vk_api.VkApi(login, password, api_version, app_id, client_secret)
     vk_session.auth()
     vk = vk_session.get_api()
@@ -28,13 +28,35 @@ def sender_my_mesages():
         try:
             vk.messages.send(user_id = int(f"{usr[0]}"), message = f"Здравствуйте {usr[1]} {usr[2]}. Вам предварительно одобрен займ, до 30 000 руб. Звоните: +7-921-444-73-44", random_id = random_mess_id)
             cursor.execute(f"UPDATE kirishiVkUsers SET message_send={random_mess_id} WHERE id={usr[0]}")
-            print(f"Сообщение пользователю {usr[0]} отправленно")
-        except Exception as e:
-            print(e)
-            #cursor.execute(f"UPDATE kirishiVkUsers SET message_send = 1 WHERE id = {usr[0]}")
-            #print(f"Сообщение пользователю {usr[0]} не отправленно")
+            logger.info(f"Сообщение пользователю {usr[0]} отправленно")
+        except:
+            logger.exception(f"Сообщение пользователю {usr[0]} не отправленно")
             continue
         conn.commit()
         time.sleep(100)
 
-sender_my_mesages()
+
+# ПОЛУЧАЕМ ЛОГИ
+def get_logs():
+    log_filename = 'messages.log'
+    fmt = logging.Formatter('%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s')
+
+    file_handler = logging.FileHandler(filename=log_filename)
+    file_handler.setFormatter(fmt)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(fmt)
+
+    root_logger = logging.getLogger()
+
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(stdout_handler)
+
+    root_logger.setLevel(logging.DEBUG)
+
+    return root_logger
+
+
+if __name__ == "__main__":
+    root_logger = get_logs()
+    sender_my_mesages()
